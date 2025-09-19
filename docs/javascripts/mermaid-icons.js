5d426898488f73fe9206d7bba43e75345926b041
Christian Ziobro <ziobro44@gmail.com>
2025-09-19 00:11:04 -0700

    update structure

--- docs/javascripts/mermaid-icons.js
+++
@@ -1,8 +0,0 @@
-import mermaid from 'CDN/mermaid.esm.mjs';
-mermaid.registerIconPacks([
-  {
-    name: 'logos',
-    loader: () =>
-      fetch('https://unpkg.com/@iconify-json/logos@1/icons.json').then((res) => res.json()),
-  },
-]);
